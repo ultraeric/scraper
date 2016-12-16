9676d5a656faafd5a10d79e81d2b69d46975fe73
ultraeric <yiqi.hou@berkeley.edu>
2016-12-16 11:31:04 -0800

    Removed README

--- setup_env.sh
+++ setup_env.sh
@@ -1,36 +1,44 @@
 #!/usr/bin/env bash
 
-sudo apt-get purge runit
-sudo apt-get purge git-all
-sudo apt-get purge git
-sudo apt-get autoremove
-sudo apt update
-sudo apt install git
+sudo apt-get purge runit -y
+sudo apt-get purge git-all -y
+sudo apt-get purge git -y
+sudo apt-get autoremove -y
+sudo apt update -y
+sudo apt install git -y
 
-if command -v python3 > /dev/null 2>&1; then
-	echo "Python3 installed"
-else
-	echo "Please install Python3"
-	exit
-fi
-
-echo "\nInstalling Selenium \n"
+printf "\nInstalling Selenium \n"
 sudo apt-get install python3-pip -y
 
 python3 -m pip install selenium
 
-echo "\nInstalling Rust \n"
+printf "\nInstalling Rust \n"
 curl -sf -L https://static.rust-lang.org/rustup.sh | sh
 
-echo "\nInstalling Chrome \n"
-sudo apt-get install chromium-browser
-sudo apt-get install -f
-sudo apt-get install xvfb
+printf "\nInstalling Chromium \n"
+sudo apt-get install chromium-browser -y
+sudo apt-get install -f -y
+sudo apt-get install xvfb -y
 
-echo "\nInstalling chromedriver \n"
+printf "\nInstalling chromedriver \n"
 cd /usr/bin
 wget -N http://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip -P ~/Downloads
 sudo unzip ~/Downloads/chromedriver_linux64.zip -d ~/Downloads
 sudo chmod a+x ~/Downloads/chromedriver
 sudo mv -f ~/Downloads/chromedriver /usr/bin/chromedriver
 
+printf "\nInstalling Torch \n"
+git clone https://github.com/torch/distro.git ~/torch --recursive
+cd ~/torch; bash install-deps;
+./install.sh
+source ~/.bashrc
+luarocks install lua-cjson
+luarocks install nn
+luarocks install rnn
+luarocks install nngraph
+luarocks install dp
+luarocks install dpnn
+luarocks install graphnn
+luarocks install optim
+luarocks install unsup
+luarocks install qttorch
