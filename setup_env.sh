14fa6362e60473ba1460011d022793ae1639ca40
Eric Hou <erichou2004@gmail.com>
2016-12-19 01:17:26 -0800

    Removed unneeded environment libraries (selenium, chromium, chromedriver, and dependencies) and added python3 http library

--- setup_env.sh
+++ setup_env.sh
@@ -7,25 +7,7 @@ sudo apt-get autoremove -y
 sudo apt update -y
 sudo apt install git -y
 
-printf "\nInstalling Selenium \n"
 sudo apt-get install python3-pip -y
-
-python3 -m pip install selenium
-
-printf "\nInstalling Rust \n"
-curl -sf -L https://static.rust-lang.org/rustup.sh | sh
-
-printf "\nInstalling Chromium \n"
-sudo apt-get install chromium-browser -y
-sudo apt-get install -f -y
-sudo apt-get install xvfb -y
-
-printf "\nInstalling chromedriver \n"
-cd /usr/bin
-wget -N http://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip -P ~/Downloads
-sudo unzip ~/Downloads/chromedriver_linux64.zip -d ~/Downloads
-sudo chmod a+x ~/Downloads/chromedriver
-sudo mv -f ~/Downloads/chromedriver /usr/bin/chromedriver
 
 printf "\nInstalling Torch \n"
 git clone https://github.com/torch/distro.git ~/torch --recursive
@@ -42,3 +24,6 @@ luarocks install graphnn
 luarocks install optim
 luarocks install unsup
 luarocks install qttorch
+
+printf "\nInstalling Requests \n"
+python3 -m pip install requests
